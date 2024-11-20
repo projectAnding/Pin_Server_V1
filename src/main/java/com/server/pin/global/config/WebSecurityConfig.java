0360ae913c0825319f11dfae445ceacaba64b595
Simseoh <seohoon0707@gmail.com>
2024-11-21 08:39:43 +0900

    fix: cors Error

--- src/main/java/com/server/pin/global/config/WebSecurityConfig.java
+++ src/main/java/com/server/pin/global/config/WebSecurityConfig.java
@@ -1,4 +1,18 @@
 package com.server.pin.global.config;
 
-public class WebSecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebSecurityConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // 허용할 Origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+                .allowedHeaders("*") // 허용할 헤더
+                .allowCredentials(true); // 인증 정보 포함 여부
+    }
 }
